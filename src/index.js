604523f20d82bdcfe53807fbeee8f044e308cb46
Fahad <fahadbinqaiser3@gmail.com>
2024-07-26 18:24:24 +0500

    error handler and api response

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log(error)
+        throw error
+})
+    app.listen("process.env.PORT || 8000", ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection Failed", err)
+})
 
 
 
